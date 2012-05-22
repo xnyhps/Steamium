73adf37d074b9d8eba37ca15ebfafa02c501b98d
Thijs Alkemade <thijsalkemade@gmail.com>
2012-05-22 16:10:22 +0200

    Added Plugin classes.

---
+++ Source/AISTPlugin.h
@@ -0,0 +1,13 @@
+//
+//  AISTPlugin.h
+//  Steamium
+//
+//  Created by Thijs Alkemade on 22-05-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AISTPlugin : AIPlugin
+
+@end
---
+++ Source/AISTPlugin.m
@@ -0,0 +1,13 @@
+//
+//  AISTPlugin.m
+//  Steamium
+//
+//  Created by Thijs Alkemade on 22-05-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AISTPlugin.h"
+
+@implementation AISTPlugin
+
+@end
